0ed3d25895660f300c562f8106d37812f7ce0335
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-05-29 12:36:44 +0000

    debugging task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,17 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const data = 'The API response data';
-      resolve(data);
-    }, 1000);
+    // Getting an API call with a chance of failure
+    const ApiResponse = () => {
+      // Introduce a random chance to getting API failure
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        resolve('API response data');
+      } else {
+        reject(new Error('Failed API response'));
+      }
+    };
+
+    setTimeout(ApiResponse, 1000);
   });
 }
 
