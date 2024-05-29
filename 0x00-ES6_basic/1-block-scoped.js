1a677c19762974902dc2b701b2e996219dafc7ea
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-05-29 08:32:10 +0000

    Task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+export default function taskBlock(trueOrFalse) {
+    var task = false;
+    var task2 = true;
+  
+    if (trueOrFalse) {
+      const newTask = false;
+      const newTask2 = true;
+      task = newTask;
+      task2 = newTask2;
+    }
+  
+    return [task, task2];
+  }
