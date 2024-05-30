704bd2b02b6070c8a999bbf5408eb0c7212299e6
FAVOUR MICHAEL <62070465+fae713@users.noreply.github.com>
2024-05-30 04:29:42 +0000

    Added rejection handling to 1-main.py file

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,4 +1,19 @@
 import getFullResponseFromAPI from './1-promise';
 
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
+// Handling a successful response
+getFullResponseFromAPI(true)
+.then(response => {
+    console.log('Success:', response); // Logs the success response
+  })
+.catch(error => {
+    console.error('Error:', error.message);
+  });
+
+// Handling a failed response
+getFullResponseFromAPI(false)
+.then(response => {
+    console.log('Success:', response);
+  })
+.catch(error => {
+    console.error('Error:', error.message); // Logs the error message
+  });
