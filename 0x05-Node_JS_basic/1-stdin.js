a77ad5ba1589f78e41b9666700c0c15bd719cad9
fae713 <favourmichael47@gmail.com>
2024-07-23 17:48:11 +0100

    debugged task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,17 @@
+//stdin
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// take input from stdin
 
+// Take input from stdin
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  // print user input to console.
+  // Print user input to console
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
-    // process.exit()
+    process.exit();
   }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
