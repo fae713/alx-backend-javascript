efc7b248416211f8e7482897c6633981beaeaaf6
fae713 <favourmichael47@gmail.com>
2024-07-23 17:47:32 +0100

    Rewrote the codes using readable

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,14 @@
-// stdin task
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+// take input from stdin
 
-process.stdin.setEncoding('utf-8');
-
-// Read the user's input
-process.stdin.on('data', (input) => {
-  // Display the user's name
-  console.log(`Your name is: ${input}`);
-
-  // Close the input stream
-  process.stdin.end();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  // print user input to console.
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+    // process.exit()
+  }
 });
-
-// When the input stream is closed
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
