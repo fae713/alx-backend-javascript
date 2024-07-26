3514ad3dd33b939f703584c5800297b06ccb461c
fae713 <favourmichael47@gmail.com>
2024-07-26 16:15:15 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,13 @@
-// api.test.js
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
   it('returns the correct status code and result', (done) => {
-    request('http://localhost:7865', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome to the payment system');
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
