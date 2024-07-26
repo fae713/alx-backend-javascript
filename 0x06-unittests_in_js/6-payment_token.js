c7a559d18fbdbe66801bb1817931eb3ce253d25a
fae713 <favourmichael47@gmail.com>
2024-07-26 15:08:00 +0100

    task 6

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,13 @@
+// 6-payment_token.js
+const getPaymentTokenFromAPI = (success) => {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({ data: 'Successful response from the API' });
+      } else {
+        // Do nothing
+      }
+    });
+  };
+  
+  module.exports = getPaymentTokenFromAPI;
+  
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+// 6-payment_token.test.js
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return true if successful', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
